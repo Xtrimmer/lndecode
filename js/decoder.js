6c2d22304546cc17878634eb2bd58f2fdb93da8c
Jeff Trimmer <trimmerjeff@gmail.com>
2018-06-14 17:14:43 -0700

    Added some validation

--- js/decoder.js
+++ js/decoder.js
@@ -1,17 +1,17 @@
 //TODO - A reader MUST check that the signature is valid (see the n tagged field)
 //TODO - Tagged part of type f: the fallback on-chain address should be decoded into an address format
-//TODO - A reader MUST skip over unknown fields, an f field with unknown version, or a p, h, or n field that does not have data_length 52, 52, or 53 respectively.
 //TODO - A reader MUST check that the SHA-2 256 in the h field exactly matches the hashed description.
 //TODO - A reader MUST use the n field to validate the signature instead of performing signature recovery if a valid n field is provided.
 
-function decode(str) {
-    let input = str.toLowerCase();
+function decode(paymentRequest) {
+    let input = paymentRequest.toLowerCase();
     let splitPosition = input.lastIndexOf('1');
     let humanReadablePart = input.substring(0, splitPosition);
-    let data = input.substring(splitPosition + 1, input.length);
-    let checksum = data.substring(data.length - 6, data.length);
-    if (!verify_checksum(humanReadablePart, bech32ToFiveBitArray(data))) throw 'Malformed request: checksum invalid';
-    data = data.substring(0, data.length - 6);
+    let data = input.substring(splitPosition + 1, input.length - 6);
+    let checksum = input.substring(input.length - 6, input.length);
+    if (!verify_checksum(humanReadablePart, bech32ToFiveBitArray(data + checksum))) {
+        throw 'Malformed request: checksum is incorrect'; // A reader MUST fail if the checksum is incorrect.
+    }
     return {
         'human_readable_part': decodeHumanReadablePart(humanReadablePart),
         'data': decodeData(data, humanReadablePart),
@@ -19,16 +19,16 @@ function decode(str) {
     }
 }
 
-function decodeHumanReadablePart(str) {
+function decodeHumanReadablePart(humanReadablePart) {
     let prefixes = ['lnbc', 'lntb', 'lnbcrt'];
     let prefix;
     prefixes.forEach(value => {
-        if (str.substring(0, value.length) === value) {
+        if (humanReadablePart.substring(0, value.length) === value) {
             prefix = value;
         }
     });
-    if (prefix == null) throw 'Malformed request: undefined prefix';
-    let amount = decodeAmount(str.substring(prefix.length, str.length));
+    if (prefix == null) throw 'Malformed request: unknown prefix'; // A reader MUST fail if it does not understand the prefix.
+    let amount = decodeAmount(humanReadablePart.substring(prefix.length, humanReadablePart.length));
     return {
         'prefix': prefix,
         'amount': amount
@@ -67,13 +67,17 @@ function decodeSignature(signature) {
 function decodeAmount(str) {
     let multiplier = str.charAt(str.length - 1);
     let amount = str.substring(0, str.length - 1);
-    if (amount.substring(0, 1) === '0') throw 'Malformed request: amount cannot contain leading zeros';
+    if (amount.substring(0, 1) === '0') {
+        throw 'Malformed request: amount cannot contain leading zeros';
+    }
     amount = Number(amount);
-    if (amount < 0 || !Number.isInteger(amount)) throw 'Malformed request: amount must be a positive decimal integer';
+    if (amount < 0 || !Number.isInteger(amount)) {
+        throw 'Malformed request: amount must be a positive decimal integer'; // A reader SHOULD fail if amount contains a non-digit
+    }
 
     switch (multiplier) {
         case '':
-            return 'Any amount accepted';
+            return 'Any amount'; // A reader SHOULD indicate if amount is unspecified
         case 'p':
             return amount / 10;
         case 'n':
@@ -83,6 +87,7 @@ function decodeAmount(str) {
         case 'm':
             return amount * 100000000;
         default:
+            // A reader SHOULD fail if amount is followed by anything except a defined multiplier.
             throw 'Malformed request: undefined amount multiplier';
     }
 }
@@ -91,7 +96,7 @@ function decodeTags(tagData) {
     let tags = extractTags(tagData);
     let decodedTags = [];
     tags.forEach((value) => {
-        decodedTags.push(decodeTag(value.type, value.data));
+        decodedTags.push(decodeTag(value.type, value.length, value.data));
     });
     return decodedTags;
 }
@@ -102,55 +107,70 @@ function extractTags(str) {
         let type = str.charAt(0);
         let dataLength = bech32ToInt(str.substring(1, 3));
         let data = str.substring(3, dataLength + 3);
-        tags.push({'type': type, 'length': dataLength, 'data': data});
+        tags.push({
+            'type': type,
+            'length': dataLength,
+            'data': data
+        });
         str = str.substring(3 + dataLength, str.length);
     }
     return tags;
 }
 
-function decodeTag(type, data) {
+function decodeTag(type, length, data) {
     switch (type) {
         case 'p':
+            if (length !== 52) break; // A reader MUST skip over a 'p' field that does not have data_length 52
             return {
                 'type': type,
+                'length': length,
                 'description': 'payment_hash',
                 'value': byteArrayToHexString(fiveBitArrayTo8BitArray(bech32ToFiveBitArray(data)))
             };
         case 'd':
             return {
                 'type': type,
+                'length': length,
                 'description': 'description',
                 'value': bech32ToUTF8String(data)
             };
         case 'n':
+            if (length !== 53) break; // A reader MUST skip over a 'n' field that does not have data_length 53
             return {
                 'type': type,
-                'description': 'payee_public_ey',
+                'length': length,
+                'description': 'payee_public_key',
                 'value': byteArrayToHexString(fiveBitArrayTo8BitArray(bech32ToFiveBitArray(data)))
             };
         case 'h':
+            if (length !== 52) break; // A reader MUST skip over a 'h' field that does not have data_length 52
             return {
                 'type': type,
+                'length': length,
                 'description': 'description_hash',
                 'value': data
             };
         case 'x':
             return {
                 'type': type,
+                'length': length,
                 'description': 'expiry',
                 'value': bech32ToInt(data)
             };
         case 'c':
             return {
                 'type': type,
+                'length': length,
                 'description': 'min_final_cltv_expiry',
                 'value': bech32ToInt(data)
             };
         case 'f':
             let version = bech32ToFiveBitArray(data.charAt(0))[0];
+            if (version < 0 || version > 18) break; // a reader MUST skip over an f field with unknown version.
             data = data.substring(1, data.length);
             return {
                 'type': type,
+                'length': length,
                 'description': 'fallback_address',
                 'value': {
                     'version': version,
@@ -166,6 +186,7 @@ function decodeTag(type, data) {
             let cltvExpiryDelta = data.slice(49, 51);
             return {
                 'type': type,
+                'length': length,
                 'description': 'routing_information',
                 'value': {
                     'public_key': byteArrayToHexString(pubkey),
@@ -174,7 +195,9 @@ function decodeTag(type, data) {
                     'fee_proportional_millionths': byteArrayToInt(feeProportionalMillionths),
                     'cltv_expiry_delta': byteArrayToInt(cltvExpiryDelta)
                 }
-            }
+            };
+        default:
+        // reader MUST skip over unknown fields
     }
 }
 
